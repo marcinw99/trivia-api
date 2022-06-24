1d079d2b17f1666aee3e5fafa6c87e1eb15eb1ee
Marcin Wąsik <marcin.wasik@ridedott.com>
2022-06-24 12:47:22 +0200

    test: test get_categories endpoint

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -1,10 +1,10 @@
-import os
-import unittest
 import json
+import unittest
+
 from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
-from models import setup_db, Question, Category
+from models import setup_db
 
 
 class TriviaTestCase(unittest.TestCase):
@@ -15,7 +15,8 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}/{}".format('localhost:5432',
+                                                         self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,10 +25,26 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
+        with self.app.app_context():
+            self.db.drop_all()
         pass
+
+    def test_get_accessories_success(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(len(data['categories']), 6)
+        self.assertEqual(data['categories'][0]['id'], 1)
+        self.assertEqual(data['categories'][0]['type'], 'Science')
+
+    def test_get_accessories_failure_not_get_method(self):
+        res = self.client().post('/categories')
+
+        self.assertEqual(res.status_code, 405)
 
     """
     TODO
